dc6411d77109c98d031cd813698caa9212fd61b6
Dennis Ries <dtries@gmail.com>
2019-04-22 22:54:34 -0600

    Landing page text updated

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -17,12 +17,9 @@ class Dashboard extends Component {
                 <div className="row">
                     <div className="col s12 center-align">
                         <h4>
-                            <b>Hey there,</b> {user.name.split(" ")[0]}
+                            {user.name.split(" ")[0]}
                             <p className="flow-tect grey-text text-darken-1">
-                                You are logged into a full-stack{" "}
-                                <span style={{ fontFamily: "monospace"}}> 
-                                    MERN
-                                </span> app 👏
+                                Let's Get Started!
                             </p>
                         </h4>
                         <button 
