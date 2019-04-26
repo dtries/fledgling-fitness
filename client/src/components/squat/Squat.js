e25d90f5c6422073cf71b67d5f698ce9f4403068
Dennis Ries <dtries@gmail.com>
2019-04-26 11:35:23 -0600

    situp and squat pages basic layout complete

--- client/src/components/squat/Squat.js
+++ client/src/components/squat/Squat.js
@@ -13,15 +13,82 @@ class Squat extends Component {
         const { user } = this.props.auth;
 
         return (
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
+            <div className="container">
                 <div className="row">
                     <div className="col s12 center-align">
-                        <h4>
+                        <h4 className="user-workout">
                             {user.name.split(" ")[0]}
-                            <p className="flow-tect grey-text text-darken-1">
-                                Squats Now!
-                            </p>
                         </h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="walking-list col s12 center-align">
+                        <ul className=" work-out-list">
+                            <li className="workout-header"><h4>WEEKLY SQUAT WORKOUTS</h4>
+                            </li>
+                            <li className="workout-item">
+                                <div className="card workout-card">
+                                    <div className="card-content">
+                                        <p className="card-title" id="workout-card-title">Day 1</p>
+                                        <p>Complete 3 Sets of SQUATS:</p>
+                                        <p>60 seconds rest between sets</p>
+                                            <ul className="collection set-card">
+                                                <li className="collection-item">Set 1: 11 squats</li>
+                                                <li className="collection-item">Set 2: 12 squats</li>
+                                                <li className="collection-item">Set 3: 11 squats</li>
+                                            </ul>
+                                    </div>
+
+                                    <div className="card-action">
+                                        <button className="attempted-btn btn btn-small waves-effect waves-light hoverable">Attempted</button>
+                                        <button className="completed-btn btn btn-small waves-effect waves-light hoverable">Completed</button>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="workout-item">
+                                <div className="card workout-card">
+                                    <div className="card-content">
+                                        <p className="card-title" id="workout-card-title">Day 2</p>
+                                        <p>Complete 3 Sets of Pushups:</p>
+                                        <p>60 seconds rest between sets</p>
+                                            <ul className="collection set-card">
+                                                <li className="collection-item">Set 1: 11 squats</li>
+                                                <li className="collection-item">Set 2: 12 squats</li>
+                                                <li className="collection-item">Set 3: 12 squats</li>
+                                            </ul>                                    
+                                    </div>
+
+                                    <div className="card-action">
+                                        <button className="attempted-btn btn btn-small waves-effect waves-light hoverable">Attempted</button>
+                                        <button className="completed-btn btn btn-small waves-effect waves-light hoverable">Completed</button>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="workout-item">
+                                <div className="card workout-card">
+                                    <div className="card-content">
+                                        <p className="card-title" id="workout-card-title">Day 3</p>
+                                        <p>Complete 3 Sets of Pushups:</p>
+                                        <p>60 seconds rest between sets</p>
+                                            <ul className="collection set-card">
+                                                <li className="collection-item">Set 1: 12 squats</li>
+                                                <li className="collection-item">Set 2: 12 squats</li>
+                                                <li className="collection-item">Set 3: 12 squats</li>
+                                            </ul>                                    
+                                    </div>
+
+                                    <div className="card-action">
+                                        <button className="attempted-btn btn btn-small waves-effect waves-light hoverable">Attempted</button>
+                                        <button className="completed-btn btn btn-small waves-effect waves-light hoverable">Completed</button>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <br />
+                <div className="row">
+                    <div className="col s12 workout-card-logout">
                         <button 
                             style={{ 
                                 width: "200px",
@@ -30,13 +97,14 @@ class Squat extends Component {
                                 marginTop: "1rem"
                             }}
                             onClick={this.onLogoutClick}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                            className="workout-logout-btn btn btn-large waves-effect waves-light hoverable"
                         >
-                            Hey Squats!
+                            Logout
                         </button>
                     </div>
                 </div>
             </div>
+
         );
     }
 }
